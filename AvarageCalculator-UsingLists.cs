030c8a3509e02081fadba9cc7102386c2110bd28
JavierE195 <ellerbrockjavier@hotmail.com>
2017-03-20 21:31:00 -0500

    Add files via upload

---
+++ AvarageCalculator-UsingLists.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication30
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int>();
+
+            Console.WriteLine("Insert the number of elements in the list: ");
+            int numberElements = int.Parse(Console.ReadLine());
+            double y = 0;
+
+            for (int i = 0; i < numberElements; i++)
+            {
+                Console.Write("Insert element number " + (i + 1) + " : ");
+                double individualElement = double.Parse(Console.ReadLine());
+                y = individualElement + y;
+
+            }
+
+            double total = y / numberElements;
+
+            Console.WriteLine("The avarage of the numbers is: " + total);
+            Console.ReadKey();
+        }
+    }
+}
